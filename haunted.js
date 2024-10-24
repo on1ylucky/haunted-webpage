08bd31ad17182c663696b6c216a13c0466951039
on1ylucky <64435252+on1ylucky@users.noreply.github.com>
2024-10-24 15:28:58 -0500

    Update haunted.js

--- haunted.js
+++ haunted.js
@@ -1,2 +1,4 @@
 var spookyMessage = "The spirits have left their mark...";
-var password = "aDR1bnRlZGg0Y2tlcjEw";
+// The spirits whisper the password: aDR1bnRlZGg0Y2tlcjEw
+
+// This script appears harmless, but it's hiding something...
